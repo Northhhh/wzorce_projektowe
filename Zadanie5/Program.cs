28ab5b2cfbe136a44df16a2c85b5073fe443ae99
Kacper Brzycki <brzycki20@gmail.com>
2024-03-24 10:34:03 +0100

    Added ex. 5

---
+++ Zadanie5/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Zadanie5
+{
+    public interface ILegacyLogger
+    {
+        void Log(string message);
+    }
+
+    public class LegacyLogger : ILegacyLogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public interface INewLogger
+    {
+        void Log(string message);
+    }
+
+    public class NewLogger : INewLogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public class NewLoggerAdapter : ILegacyLogger
+    {
+        private readonly INewLogger _newLogger;
+
+        public NewLoggerAdapter(INewLogger newLogger)
+        {
+            _newLogger = newLogger;
+        }
+
+        public void Log(string message)
+        {
+            _newLogger.Log(message);
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            INewLogger newLogger = new NewLogger();
+            ILegacyLogger adapter = new NewLoggerAdapter(newLogger);
+
+            adapter.Log("Przykładowa wiadomość");
+            adapter.Log("Wystąpił problem!");
+            adapter.Log("Uwaga, potencjalny problem!");
+        }
+    }
+}
