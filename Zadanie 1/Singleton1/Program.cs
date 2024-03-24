edcb11b437e2956bef314c56b4bef9bd39b0efca
Kacper Brzycki <brzycki20@gmail.com>
2024-03-24 09:51:45 +0100

    Added remaining exercises

---
+++ Zadanie 1/Singleton1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Singleton1
+{
+    class Programme
+    {
+        static void Main(string[] args)
+        {
+            NotificationLog log = NotificationLog.GetInstance();
+            log.AddNotification("Notification 1");
+            log.AddNotification("Notification 2");
+
+            log.RenderNotifications();
+        }
+    }
+
+    public class NotificationLog
+    {
+        private static NotificationLog _instance;
+
+        private readonly List<string> _notifications = new List<string>();
+
+        private NotificationLog()
+        {
+        }
+
+        public static NotificationLog GetInstance()
+        {
+            if (_instance is null)
+                _instance = new NotificationLog();
+
+            return _instance;
+        }
+
+        public void AddNotification(string notification)
+        {
+            _notifications.Add(notification);
+        }
+
+        public void RenderNotifications()
+        {
+            foreach (string notification in _notifications)
+            {
+                Console.WriteLine(notification);
+            }
+
+            return;
+        }
+    }
+}
---
+++ Zadanie 2/Singleton2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Singleton_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintBuffer buffer = PrintBuffer.GetInstance();
+            buffer.AddTask("First task", "Lorem ipsum 111111111");
+            buffer.AddTask("Second task", "Lorem ipsum 22222222");
+            buffer.AddTask("Third task", "Lorem ipsum 333333333");
+
+            buffer.PrintTasks();
+        }
+    }
+
+    class PrintBuffer
+    {
+        private static PrintBuffer _instance = new PrintBuffer();
+        private readonly List<string[]> _data;
+
+        private PrintBuffer()
+        {
+            _data = new List<string[]>();
+        }
+
+        public static PrintBuffer GetInstance()
+        {
+            if (_instance is null)
+                _instance = new PrintBuffer();
+            return _instance;
+        }
+
+        public void AddTask(string name, string content)
+        {
+            _data.Add(new string[] { name, content });
+        }
+
+        public void PrintTasks()
+        {
+            foreach (var data in _data)
+            {
+                Console.WriteLine("--- Task: {0} ---", data[0]);
+                Console.WriteLine(data[1]);
+            }
+
+            _data.Clear();
+        }
+    }
+}
