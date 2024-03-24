20ae600427e3a3b98d2aeac9411ea037f3f130e0
Kacper Brzycki <brzycki20@gmail.com>
2024-03-24 09:21:24 +0100

    Added exercise 4

---
+++ Zadanie 4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Adapter1
+{
+    interface ITarget
+    {
+        void GetArea();
+    }
+
+    class Square
+    {
+        private readonly double side;
+
+        public Square(double sqSide)
+        {
+            this.side = sqSide;
+        }
+
+        public void GetArea()
+        {
+            Console.WriteLine("Obszar kwadratu wynosi " + Math.Pow(side, 2));
+        }
+    }
+
+    class SquareAdapter : ITarget
+    {
+        private Square square;
+
+        public SquareAdapter(Square square)
+        {
+            this.square = square;
+        }
+
+        public void GetArea()
+        {
+            square.GetArea();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square square = new Square(2.5);
+            SquareAdapter squareAdapter = new SquareAdapter(square);
+
+            squareAdapter.GetArea();
+        }
+    }
+}
